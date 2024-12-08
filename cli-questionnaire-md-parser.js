84acb6b44ed33632202781d64ab556a3c029a284
tomato-alex <a12028817@unet.univie.ac.at>
2024-12-08 14:44:58 +0100

    Add flexible output via strategy pattern

--- cli-questionnaire-md-parser.js
+++ cli-questionnaire-md-parser.js
@@ -1,9 +1,9 @@
-import { marked } from "marked";
 import fs from "fs";
 import { program } from "commander";
-import { MarkdownToHtml } from "./mdToHtml.js";
-import { QuestionnaireParser } from "./mdToJson.js";
-import { JsonToLss } from "./jsonToLss.js";
+import { MarkdownToHtml } from "./parserLibraries/mdToHtml.js";
+import { MarkdownToJson } from "./parserLibraries/mdToJson.js";
+import { JsonToLss } from "./parserLibraries/jsonToLss.js";
+import { QuestionnaireParser } from "./parserLibraries/questionnaireParser.js";
 
 // Command-line interface definition
 program
@@ -29,40 +29,44 @@ program
 
             // Use the new MD to Json parser to first get the initial structure
             // Why json? To add more flexability when transforming into different formats like html, lss, xml or others
-            const questionnaireParser = new QuestionnaireParser();
+            const questionnaireParser = new MarkdownToJson();
             questionnaireParser.parseData(data);
             const markdownToJson = questionnaireParser.exportData();
             let output = markdownToJson;
 
+            let jsonParser = new QuestionnaireParser();
             if (options.type === "html/depr") {
                 // Parsing for the old version of the markdown syntax.
                 // TODO implement json to Html with the new format();
-                const markdownToHtml = new MarkdownToHtml();
-                output = markdownToHtml.parseData(data);
+                jsonParser = new MarkdownToHtml();
             } else if (options.type === "lss") {
                 // Parsing for the new version of the markdown syntax.
-                const jsonToLss = new JsonToLss();
-                output = jsonToLss.parseData(markdownToJson);
+                jsonParser = new JsonToLss();
+            } else if (options.type === "html") {
+                // implement new html parser
             }
 
-            if (options.debug) {
-            }
+            output = jsonParser.parseData(output).then((output) => {
+                if (options.debug) {
+                    console.log("Jsonified Markdown > \n" + markdownToJson);
+                }
 
-            if (options.display) {
-                console.log("Entire output > \n" + output);
-            }
+                if (options.display) {
+                    console.log("Entire output > \n" + output);
+                }
 
-            if (options.output) {
-                fs.writeFile(options.output, output, (err) => {
-                    if (err) {
-                        console.error(`Error writing file: ${err.message}`);
-                        process.exit(1);
-                    }
-                    console.log(`Output written to ${options.output}`);
-                });
-            } else {
-                console.log(standardMarkdownToHTML);
-            }
+                if (options.output) {
+                    fs.writeFile(options.output, output, (err) => {
+                        if (err) {
+                            console.error(`Error writing file: ${err.message}`);
+                            process.exit(1);
+                        }
+                        console.log(`Output written to ${options.output}`);
+                    });
+                } else {
+                    console.log(output);
+                }
+            });
         });
     });
 
